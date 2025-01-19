435734d8a914d0957d9a0f2270e816d63338969a
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-01-19 17:28:10 +0800

    fix: move handlers and main into new folder structure

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/amrllkmn/apex/internal/activities"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello")
-	})
-	r.GET("/hello/:name", HandleHello)
+	r.GET("/hello/:name", activities.HandleHello)
 	r.Run() // Run and serve on 8080
 }
--- internal/activities/handler.go
+++ internal/activities/handler.go
@@ -1,4 +1,4 @@
-package main
+package activities
 
 import (
 	"net/http"
