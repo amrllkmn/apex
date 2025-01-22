533b3dc3404f045ed724af49d9c866ddf138fb41
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-01-22 13:08:37 +0800

    fix: use PATCH again

--- internal/activities/router.go
+++ internal/activities/router.go
@@ -10,7 +10,7 @@ func SetupRouter(r *gin.Engine) *gin.Engine {
 	r.POST("v1/activities", activitiesHandler.HandleCreateActivity)
 	r.GET("v1/activities", activitiesHandler.HandleGetActivities)
 	r.GET("v1/activities/:id", activitiesHandler.HandleReadActivity)
-	r.PUT("/v1/activities/:id", activitiesHandler.HandleUpdateActivity)
+	r.PATCH("/v1/activities/:id", activitiesHandler.HandleUpdateActivity)
 	r.DELETE("/v1/activities/:id", activitiesHandler.HandleDeleteActivity)
 	return r
 }
