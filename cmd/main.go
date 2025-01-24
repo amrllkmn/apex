1266c17448045a692f1cb7a906cffcf6e1e8349b
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-01-24 15:31:09 +0800

    feat: add gorm driver

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/amrllkmn/apex/internal/activities"
+	"github.com/amrllkmn/apex/internal/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r = activities.SetupRouter(r)
+	db := database.InitDB()
+	err := db.AutoMigrate(&activities.Activity{})
+	if err != nil {
+		log.Fatal("Migration failed")
+	}
+	r = activities.SetupRouter(r, db)
 	r.Run(":8081") // Run and serve on 8080
 }
